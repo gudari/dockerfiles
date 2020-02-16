7c79311732e767d1e0aea501323ed26404747b50
gudari <aartola1986@gmail.com>
2020-02-16 22:30:11 +0100

    update zookeeper rest

---
+++ zookeeper-rest/build/3.5.6/scripts/bootstrap.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function generate_rest_properties() {
+  local path=$1
+  cat > $path << EOF
+#
+# 
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# 
+#
+
+#
+# ZooKeeper REST Gateway Configuration file
+#
+# Start
+# End
+EOF
+}
+
+function addProperty() {
+  local path=$1
+  local name=$2
+  local value=$3
+
+  local entry="$name=$value"
+  local escapedEntry=$(echo $entry | sed 's/\//\\\//g')
+  sed -i "/#\ End/ s/.*/${escapedEntry}\n&/" $path
+}
+
+
+function property() {
+    local path=$1
+    local module=$2
+    local envPrefix=$3
+
+    local var
+    local value
+    
+    echo "Configuring $module"
+    for c in `printenv | grep $envPrefix | sed -e "s/^${envPrefix}_//" -e "s/=.*$//"`; do 
+        name=`echo ${c} | sed -e "s/___/-/g" -e "s/__/@/g" -e "s/_/./g" -e "s/@/_/g"`
+        var="${envPrefix}_${c}"
+        value=${!var}
+        echo " - Setting $name=$value"
+        addProperty $path $name "$value"
+    done
+}
+
+generate_rest_properties $ZOOKEEPER_REST_HOME/conf/rest.properties
+property $ZOOKEEPER_REST_HOME/conf/rest.properties zoo-rest REST_PROPERTIES
+
+#debug
+#cat $ZOOKEEPER_REST_HOME/conf/rest.properties
+
+if [[ "${HOSTNAME}" =~ "zookeeper-rest" ]]; then
+  $ZOOKEEPER_REST_HOME/zookeeper-rest.sh start-foreground
+fi
